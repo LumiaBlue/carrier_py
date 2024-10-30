e9e4a71d003a1fd4b0227fd52ce46bfa92355d93
Skye Smith <mvsmith@uvm.edu>
2024-10-30 16:24:08 -0400

    experimenting with tkinter a bit

--- tkinter tests/tkintertest.py
+++ tkinter tests/tkintertest.py
@@ -1,3 +1,23 @@
-import tkinter
+import tkinter as tk
+from tkinter import scrolledtext
 
-tkinter._test()
+root = tk.Tk()
+
+output = scrolledtext.ScrolledText(root, width = 40, height = 20)
+entry = tk.Entry(root, width = 40)
+
+output.grid(row = 0, column = 0)
+entry.grid(row = 1, column = 0)
+
+def send(event):
+    output.configure(state = tk.NORMAL)
+    output.insert(tk.INSERT, entry.get() + "\n")
+    output.configure(state = tk.DISABLED)
+    entry.delete(0, tk.END)
+
+entry.bind('<Return>', send)
+output.configure(state = tk.DISABLED)
+
+tk.mainloop()
+
+print("test")
